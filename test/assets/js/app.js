715d39134b37023048dbdc98205be5a25b4a8501
Quentin Hocdé <quentinhcd@gmail.com>
2024-03-19 13:27:52 +0100

    clean up app

--- test/assets/js/app.js
+++ test/assets/js/app.js
@@ -1,15 +1,5 @@
 import { load } from 'piecesjs';
 
-// async function dynamicLoad(src) {
-//   await import(src);
-// }
-
-// function dynamicLoad(src) {
-  // import('/assets/js/components/Add.js').then(module => {
-  //   console.log(module)
-  // });
-// }
-
 // 
 // IMPORT components
 // 
@@ -18,6 +8,3 @@ load('c-add', () => import(`/assets/js/components/Add.js`));
 load('c-header', () => import(`/assets/js/components/Header.js`));
 load('c-button', () => import(`/assets/js/components/Button.js`));
 // ------------------------------------------------------------
-
-// import('/assets/js/components/Add.js');
-// dynamicLoad('/assets/js/components/Add.js');
