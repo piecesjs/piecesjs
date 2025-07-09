4428835c80eb4675de8a6e4d4be5b6f6e84ac88f
quentinhocde <quentinhcd@gmail.com>
2025-07-09 12:42:57 +0200

    Build

--- build/piecesjs.js
+++ build/piecesjs.js
@@ -1,34 +1,34 @@
-const m = async (o, e, t = document) => {
-  t.getElementsByTagName(o).length > 0 && await e();
+const m = async (o, t, e = document) => {
+  e.getElementsByTagName(o).length > 0 && await t();
 }, h = (o) => {
-  var e = Object.prototype.toString.call(o);
-  return typeof o == "object" && /^\[object (HTMLCollection|NodeList|Object)\]$/.test(e) && typeof o.length == "number" && (o.length === 0 || typeof o[0] == "object" && o[0].nodeType > 0);
+  var t = Object.prototype.toString.call(o);
+  return typeof o == "object" && /^\[object (HTMLCollection|NodeList|Object)\]$/.test(t) && typeof o.length == "number" && (o.length === 0 || typeof o[0] == "object" && o[0].nodeType > 0);
 };
 class d {
   constructor() {
     this.loadedPiecesCount = 0, this.piecesCount = 0, this.currentPieces = {};
   }
-  addPiece(e) {
-    typeof this.currentPieces[e.name] != "object" && (this.currentPieces[e.name] = {}), this.currentPieces[e.name][e.id] = e;
+  addPiece(t) {
+    typeof this.currentPieces[t.name] != "object" && (this.currentPieces[t.name] = {}), this.currentPieces[t.name][t.id] = t;
   }
-  removePiece(e) {
-    delete this.currentPieces[e.name][e.id];
+  removePiece(t) {
+    delete this.currentPieces[t.name][t.id];
   }
 }
 let f = new d();
 class p extends HTMLElement {
-  constructor(e, { stylesheets: t = [] } = {}) {
-    super(), this.name = e || this.constructor.name, this.template = document.createElement("template"), this.piecesManager = f, this.stylesheets = t, this.updatedPiecesCount = this.piecesManager.piecesCount++, this.innerHTML != "" && (this.baseHTML = this.innerHTML), this._boundListeners = /* @__PURE__ */ new Map();
+  constructor(t, { stylesheets: e = [] } = {}) {
+    super(), this.name = t || this.constructor.name, this.template = document.createElement("template"), this.piecesManager = f, this.stylesheets = e, this.updatedPiecesCount = this.piecesManager.piecesCount++, this.innerHTML != "" && (this.baseHTML = this.innerHTML), this._boundListeners = /* @__PURE__ */ new Map();
   }
   /**
    * default function from native web components connectedCallback()
    */
-  connectedCallback(e = !0) {
-    e && (typeof this.cid == "string" ? this.cid = this.cid : this.cid = `c${this.updatedPiecesCount}`, this.piecesManager.addPiece({
+  connectedCallback(t = !0) {
+    t && (typeof this.cid == "string" ? this.cid = this.cid : this.cid = `c${this.updatedPiecesCount}`, this.piecesManager.addPiece({
       name: this.name,
       id: this.cid,
       piece: this
-    })), this.privatePremount(e), this.baseHTML == null && (this.innerHTML = "", this.template.innerHTML = this.render() != null ? this.render() : "", this.appendChild(this.template.cloneNode(!0).content)), this.privateMount(e);
+    })), this.privatePremount(t), this.baseHTML == null && (this.innerHTML = "", this.template.innerHTML = this.render() != null ? this.render() : "", this.appendChild(this.template.cloneNode(!0).content)), this.privateMount(t);
   }
   /**
    * Function to render HTML in the component. If component is not emtpy, the rendering is not called
@@ -52,20 +52,20 @@ class p extends HTMLElement {
    * Lifecycle - step : 0
    * @param { firstHit } boolean (false if it's an update)
    */
-  privatePremount(e = !0) {
-    this.baseHTML == null && (this.innerHTML = ""), this.log && console.log("🚧 premount", this.name), this.loadStyles(e), this.premount(e);
+  privatePremount(t = !0) {
+    this.baseHTML == null && (this.innerHTML = ""), this.log && console.log("🚧 premount", this.name), this.loadStyles(t), this.premount(t);
   }
   /**
    * Satelite function for premount
    */
-  premount(e = !0) {
+  premount(t = !0) {
   }
   /**
    * Lifecycle - step : 1
    * @param { firstHit } boolean (false if it's an update)
    */
-  privateMount(e) {
-    if (this.log && console.log("✅ mount", this.name), e) {
+  privateMount(t) {
+    if (this.log && console.log("✅ mount", this.name), t) {
       this.piecesManager.loadedPiecesCount++, this.domEventsElements = Array.from(this.querySelectorAll("*")).filter(
         (i) => {
           const s = i.attributes;
@@ -75,9 +75,9 @@ class p extends HTMLElement {
           return !1;
         }
       );
-      const t = this.attributes;
-      for (let i = 0; i < t.length; i++)
-        t[i].name.startsWith("data-events-") && this.domEventsElements.push(this);
+      const e = this.attributes;
+      for (let i = 0; i < e.length; i++)
+        e[i].name.startsWith("data-events-") && this.domEventsElements.push(this);
       this.domEventsElements && this.domEventsElements.forEach((i) => {
         let s = i.attributes;
         for (let n = 0; n < s.length; n++)
@@ -91,18 +91,18 @@ class p extends HTMLElement {
               a = l[0];
               const c = l[1], u = l[2];
               i.dataset.eventInit = !0, this.on(r, i, () => {
-                this.call(a, {}, c, u);
+                this.call(a, i, c, u);
               });
             }
           }
       });
     }
-    this.mount(e);
+    this.mount(t);
   }
   /**
    * Satelite function for mount
    */
-  mount(e = !0) {
+  mount(t = !0) {
   }
   /**
    * Lifecycle - step : 2
@@ -119,33 +119,33 @@ class p extends HTMLElement {
    * Lifecycle - step : 3
    * @param { update } boolean
    */
-  privateUnmount(e = !1) {
-    e || (this.piecesManager.removePiece({
+  privateUnmount(t = !1) {
+    t || (this.piecesManager.removePiece({
       name: this.name,
       id: this.cid
-    }), this.domEventsElements && this.domEventsElements.forEach((t) => {
-      let i = t.attributes;
+    }), this.domEventsElements && this.domEventsElements.forEach((e) => {
+      let i = e.attributes;
       for (let s = 0; s < i.length; s++)
         if (i[s].name.startsWith("data-events-")) {
           const n = i[s].name.replace("data-events-", "");
           let r = i[s].value;
           const a = i[s].value.split(",");
           if (a.length == 1)
-            typeof this[r] == "function" && this.off(n, t, this[r]);
-          else if (a.length >= 2 && t.dataset.eventInit == null) {
+            typeof this[r] == "function" && this.off(n, e, this[r]);
+          else if (a.length >= 2 && e.dataset.eventInit == null) {
             r = a[0];
             const l = a[1], c = a[2];
-            t.dataset.eventInit = !0, this.off(n, t, () => {
-              this.call(r, t, l, c);
+            e.dataset.eventInit = !0, this.off(n, e, () => {
+              this.call(r, e, l, c);
             });
           }
         }
-    })), this.log && console.log("❌ unmount", this.name), this.unmount(e);
+    })), this.log && console.log("❌ unmount", this.name), this.unmount(t);
   }
   /**
    * Satelite function for unmount
    */
-  unmount(e = !1) {
+  unmount(t = !1) {
   }
   /**
    * default function from native web components
@@ -153,39 +153,39 @@ class p extends HTMLElement {
    * @param { String } oldValue
    * @param { String } newValue
    */
-  attributeChangedCallback(e, t, i) {
-    t !== i && (this[e] = i, this.privateUpdate());
+  attributeChangedCallback(t, e, i) {
+    e !== i && (this[t] = i, this.privateUpdate());
   }
   /**
    * @param { String } query
    * @param { HTMLElement } context
    */
-  $(e, t = this) {
-    const i = t.querySelectorAll(e);
+  $(t, e = this) {
+    const i = e.querySelectorAll(t);
     return i.length == 1 ? i[0] : i.length == 0 ? null : i;
   }
-  dom(e, t = this) {
-    const i = t.querySelectorAll(e);
+  dom(t, e = this) {
+    const i = e.querySelectorAll(t);
     return i.length == 1 ? i[0] : i.length == 0 ? null : i;
   }
   // To capture element using data-attribute <div data-dom='query'></div>
-  domAttr(e, t = this) {
-    const i = t.querySelectorAll(`[data-dom="${e}"]`);
+  domAttr(t, e = this) {
+    const i = e.querySelectorAll(`[data-dom="${t}"]`);
     return i.length == 1 ? i[0] : i.length == 0 ? null : i;
   }
-  $All(e, t = this) {
-    return Array.from(t.querySelectorAll(e));
+  $All(t, e = this) {
+    return Array.from(e.querySelectorAll(t));
   }
-  domAll(e, t = this) {
-    return Array.from(t.querySelectorAll(e));
+  domAll(t, e = this) {
+    return Array.from(e.querySelectorAll(t));
   }
-  domAttrAll(e, t = this) {
-    return Array.from(t.querySelectorAll(`[data-dom="${e}"]`));
+  domAttrAll(t, e = this) {
+    return Array.from(e.querySelectorAll(`[data-dom="${t}"]`));
   }
-  captureTree(e = this) {
-    const t = this.querySelectorAll("[data-dom]");
+  captureTree(t = this) {
+    const e = this.querySelectorAll("[data-dom]");
     let i = {};
-    for (let s of t) {
+    for (let s of e) {
       const n = s.getAttribute("data-dom");
       typeof i[n] > "u" && (i[n] = []), i[n].push(s);
     }
@@ -201,9 +201,9 @@ class p extends HTMLElement {
    * @param { function } func
    * @param { Object } params
    */
-  on(e, t, i, s = null) {
-    if (t != null) {
-      const n = `${e}_${i.name}`;
+  on(t, e, i, s = null) {
+    if (e != null) {
+      const n = `${t}_${i.name}`;
       if (!this._boundListeners.has(n)) {
         const a = i.bind(this);
         this._boundListeners.set(n, {
@@ -212,9 +212,9 @@ class p extends HTMLElement {
         });
       }
       const r = this._boundListeners.get(n).bound;
-      h(t) || Array.isArray(t) ? t.length > 0 && t.forEach((a) => {
-        s == null ? a.addEventListener(e, r) : a.addEventListener(e, () => r(s));
-      }) : s == null ? t.addEventListener(e, r) : t.addEventListener(e, () => r(s));
+      h(e) || Array.isArray(e) ? e.length > 0 && e.forEach((a) => {
+        s == null ? a.addEventListener(t, r) : a.addEventListener(t, () => r(s));
+      }) : s == null ? e.addEventListener(t, r) : e.addEventListener(t, () => r(s));
     }
   }
   /**
@@ -223,17 +223,17 @@ class p extends HTMLElement {
    * @param { HTMLElement } el
    * @param { function } func
    */
-  off(e, t, i) {
-    if (t != null) {
-      const s = `${e}_${i.name}`, n = this._boundListeners.get(s);
+  off(t, e, i) {
+    if (e != null) {
+      const s = `${t}_${i.name}`, n = this._boundListeners.get(s);
       if (!n) {
         console.warn(`No bound listener found for ${s}`);
         return;
       }
       const r = n.bound;
-      h(t) || Array.isArray(t) ? t.length > 0 && t.forEach((a) => {
-        a.removeEventListener(e, r);
-      }) : t.removeEventListener(e, r), this._boundListeners.delete(s);
+      h(e) || Array.isArray(e) ? e.length > 0 && e.forEach((a) => {
+        a.removeEventListener(t, r);
+      }) : e.removeEventListener(t, r), this._boundListeners.delete(s);
     }
   }
   /**
@@ -242,11 +242,11 @@ class p extends HTMLElement {
    * @param { HTMLElement } el, by default the event is emit on document
    * @param { Object } params
    */
-  emit(e, t = document, i) {
-    const s = new CustomEvent(e, {
+  emit(t, e = document, i) {
+    const s = new CustomEvent(t, {
       detail: i
     });
-    t.dispatchEvent(s);
+    e.dispatchEvent(s);
   }
   /**
    * Call function of a component, from a component
@@ -255,20 +255,20 @@ class p extends HTMLElement {
    * @param { String } pieceName
    * @param { String } pieceId
    */
-  call(e, t, i, s) {
+  call(t, e, i, s) {
     Object.keys(this.piecesManager.currentPieces).forEach((n) => {
       n == i && Object.keys(this.piecesManager.currentPieces[n]).forEach((r) => {
-        s != null ? r == s && this.piecesManager.currentPieces[n][r].piece[e](t) : this.piecesManager.currentPieces[n][r].piece[e](t);
+        s != null ? r == s && this.piecesManager.currentPieces[n][r].piece[t](e) : this.piecesManager.currentPieces[n][r].piece[t](e);
       });
     });
   }
   /**
    * Dynamic loading of stylesheets from super()
    */
-  async loadStyles(e = !0) {
-    if (e)
-      for (let t = 0; t < this.stylesheets.length; t++)
-        await this.stylesheets[t]();
+  async loadStyles(t = !0) {
+    if (t)
+      for (let e = 0; e < this.stylesheets.length; e++)
+        await this.stylesheets[e]();
   }
   get log() {
     return typeof this.getAttribute("log") == "string";
@@ -276,11 +276,11 @@ class p extends HTMLElement {
   get cid() {
     return this.getAttribute("cid");
   }
-  set cid(e) {
-    return this.setAttribute("cid", e);
+  set cid(t) {
+    return this.setAttribute("cid", t);
   }
   get properties() {
-    return Object.values(this.attributes).map((e) => `${e.name}="${e.value}"`).join(" ");
+    return Object.values(this.attributes).map((t) => `${t.name}="${t.value}"`).join(" ");
   }
 }
 export {
